2b9f6cb52a803d59e03c2383f5fd857f8d895344
GramaAdrian <grama.adi2044@yahoo.com>
2024-09-03 15:18:17 +0300

    commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+baguetteBox.run('.gallery');
+
+class Filter {
+  constructor(inputId, dataAttribute) {
+    this.input = document.getElementById(inputId);
+    this.dataAttribute = dataAttribute;
+    this.items = document.querySelectorAll(`[${dataAttribute}]`);
+
+    this.init();
+  }
+
+  init() {
+    this.input.addEventListener('input', () => this.filterItems());
+  }
+
+  filterItems() {
+    const query = this.input.value.toLowerCase();
+
+    this.items.forEach(item => {
+      const attributeValue = item.getAttribute(this.dataAttribute).toLowerCase();
+      if (attributeValue.includes(query)) {
+        item.style.display = '';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
+}
+
+const search = new Filter('search', 'data-caption');
